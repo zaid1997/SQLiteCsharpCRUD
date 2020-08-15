2e003f78c94709bd7eed5b221cafab7a668c0378
Zaid Kindman <38330943+zaid1997@users.noreply.github.com>
2020-08-15 21:53:42 +0300

    Make it calss

--- SQLiteCsharpCRUD.cs
+++ SQLiteCsharpCRUD.cs
@@ -1,94 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.SQLite;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SQLiteApp
+class SQLiteCRUD
 {
-    class Program
+    static SQLiteConnection CreateConnection()
     {
-        static void Main(string[] args)
+        SQLiteConnection sqliteConnection = new SQLiteConnection("Data Source= Database.db; Version = 3; New = True; Compress = True; ");
+        try
         {
-            SQLiteConnection sqliteConnection = CreateConnection();
-            InsertData(sqliteConnection, "Kindman", 23);
-            UpdateData(sqliteConnection, "Kindman", 24);
-            DeleteData(sqliteConnection,"Kindman", 24);
-            ReadData(sqliteConnection);
+            sqliteConnection.Open();
         }
-
-        static SQLiteConnection CreateConnection()
+        catch (Exception ex)
         {
-            SQLiteConnection sqliteConnection = new SQLiteConnection("Data Source= Database.db; Version = 3; New = True; Compress = True; ");
-            try
-            {
-                sqliteConnection.Open();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            return sqliteConnection;
+            Console.WriteLine(ex.Message);
         }
+        return sqliteConnection;
+    }
 
-        static void CreateTable(SQLiteConnection sqliteConnection)
-        {
-            try 
-            {
-                SQLiteCommand sqliteCommand = sqliteConnection.CreateCommand();
-                sqliteCommand.CommandText = "CREATE TABLE People(name VARCHAR(20), age INT)";
-                sqliteCommand.ExecuteNonQuery();
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-
-        static void InsertData(SQLiteConnection sqliteConnection, string name,int age)
-        {
-            SQLiteCommand sqliteCommand;
-            sqliteCommand = sqliteConnection.CreateCommand();
-            sqliteCommand.CommandText = "INSERT INTO People(name, age) VALUES($name,$age);";
-            sqliteCommand.Parameters.AddWithValue("$name", name);
-            sqliteCommand.Parameters.AddWithValue("$age", age);
-            sqliteCommand.ExecuteNonQuery();
-        }
-
-        static void DeleteData(SQLiteConnection sqliteConnection, string name, int age)
+    static void CreateTable(SQLiteConnection sqliteConnection)
+    {
+        try 
         {
             SQLiteCommand sqliteCommand = sqliteConnection.CreateCommand();
-            sqliteCommand.CommandText = "DELETE FROM People WHERE name=$name AND age=$age";
-            sqliteCommand.Parameters.AddWithValue("$name", name);
-            sqliteCommand.Parameters.AddWithValue("$age", age);
+            sqliteCommand.CommandText = "CREATE TABLE People(name VARCHAR(20), age INT)";
             sqliteCommand.ExecuteNonQuery();
         }
-
-        static void UpdateData(SQLiteConnection sqliteConnection, string name, int age)
+        catch(Exception ex)
         {
-            SQLiteCommand sqliteCommand = sqliteConnection.CreateCommand();
-            sqliteCommand.CommandText = "UPDATE People SET age=$age WHERE name=$name";
-            sqliteCommand.Parameters.AddWithValue("$name", name);
-            sqliteCommand.Parameters.AddWithValue("$age", age);
-            sqliteCommand.ExecuteNonQuery();
+            Console.WriteLine(ex.Message);
         }
+    }
 
-        static void ReadData(SQLiteConnection sqliteConnection)
+    static void InsertData(SQLiteConnection sqliteConnection, string name,int age)
+    {
+        SQLiteCommand sqliteCommand;
+        sqliteCommand = sqliteConnection.CreateCommand();
+        sqliteCommand.CommandText = "INSERT INTO People(name, age) VALUES($name,$age);";
+        sqliteCommand.Parameters.AddWithValue("$name", name);
+        sqliteCommand.Parameters.AddWithValue("$age", age);
+        sqliteCommand.ExecuteNonQuery();
+    }
+
+    static void DeleteData(SQLiteConnection sqliteConnection, string name, int age)
+    {
+        SQLiteCommand sqliteCommand = sqliteConnection.CreateCommand();
+        sqliteCommand.CommandText = "DELETE FROM People WHERE name=$name AND age=$age";
+        sqliteCommand.Parameters.AddWithValue("$name", name);
+        sqliteCommand.Parameters.AddWithValue("$age", age);
+        sqliteCommand.ExecuteNonQuery();
+    }
+
+    static void UpdateData(SQLiteConnection sqliteConnection, string name, int age)
+    {
+        SQLiteCommand sqliteCommand = sqliteConnection.CreateCommand();
+        sqliteCommand.CommandText = "UPDATE People SET age=$age WHERE name=$name";
+        sqliteCommand.Parameters.AddWithValue("$name", name);
+        sqliteCommand.Parameters.AddWithValue("$age", age);
+        sqliteCommand.ExecuteNonQuery();
+    }
+
+    static void ReadData(SQLiteConnection sqliteConnection)
+    {
+        SQLiteDataReader sqliteDatareader;
+        SQLiteCommand sqliteCommand = sqliteConnection.CreateCommand();
+        sqliteCommand.CommandText = "SELECT * FROM People";
+
+        sqliteDatareader = sqliteCommand.ExecuteReader();
+        while (sqliteDatareader.Read())
         {
-            SQLiteDataReader sqliteDatareader;
-            SQLiteCommand sqliteCommand = sqliteConnection.CreateCommand();
-            sqliteCommand.CommandText = "SELECT * FROM People";
-
-            sqliteDatareader = sqliteCommand.ExecuteReader();
-            while (sqliteDatareader.Read())
-            {
-                string name = sqliteDatareader.GetString(0);
-                int age = sqliteDatareader.GetInt32(1);
-                Console.WriteLine(name + ": " + age);
-            }
-            sqliteConnection.Close();
+            string name = sqliteDatareader.GetString(0);
+            int age = sqliteDatareader.GetInt32(1);
+            Console.WriteLine(name + ": " + age);
         }
-
+        sqliteConnection.Close();
     }
 }
